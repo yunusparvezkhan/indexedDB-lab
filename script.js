6c232d6983028bccb6be33988106b97eb09c3558
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-25 23:40:09 +0530

    feat(ObjectStore): create object store and indexes
    This commit creates a object store inside the database and creates two
    indexes that can filter the data through

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ request.onerror = (event) => {
     console.log("Some error occured");
     console.log(event);
 }
+
+request.onupgradeneeded = () => {
+    const db = request.result;
+    const store = db.createObjectStore("cars", { keyPath: "id" });
+    store.createIndex("cars_color", ["color"], { unique: false });
+    store.createIndex("color_and_make", ["color", "make"], { unique: false });
+};
