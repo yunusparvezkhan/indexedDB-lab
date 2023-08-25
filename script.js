70eb6c152a9f170003efd8474401d9d63f08f8ee
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-25 23:48:31 +0530

    feat(data fetch): fetch data using the unique keyPath
    This commit shows how data can be fetched from IDB stores using the unique keyPath, that
    is provided in the Object Store configuration

--- script.js
+++ script.js
@@ -20,15 +20,23 @@ request.onupgradeneeded = () => {
 };
 
 request.onsuccess = async () => {
+    // References
     const db = request.result;
     const transaction = db.transaction("cars", "readwrite");
     const store = transaction.objectStore("cars");
     const colorIndex = store.index("cars_color");
     const makeModelIndex = store.index("color_and_make");
 
+    // Data entries
     store.put({ id: 1, color: "Red", make: "Tata Motors" });
     store.put({ id: 2, color: "Orange", make: "Mahindra" });
     store.put({ id: 3, color: "Blue", make: "Range Rover" });
     store.put({ id: 4, color: "White", make: "Hindustan Motors" });
     store.put({ id: 5, color: "Red", make: "Polo" });
+
+    // Data fetching
+    // Data fetching using unique keyPath
+    const idQuery = store.get(2)
+    console.log(idQuery);
+
 }
