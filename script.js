432ab51b8fa58bdada471871195d1c93667450da
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-25 21:51:31 +0530

    style(landing): styled the landing page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Research initial setup");
+
+const indexedDB =
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
+
+const request = indexedDB.open("UsersDatabase", 1);
+
+request.onerror = (event) => {
+    console.log("Some error occured");
+    console.log(event);
+}
